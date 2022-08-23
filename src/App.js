a0336ecd9955e0dafcd7a63e0d86e04f7e63171c
Martin Trajanovski <martin.trajanovski@gmail.com>
2022-08-23 12:47:55 +0200

    feat: Test feature workflow

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Changed this text to test the workflow of feature branch
         </a>
       </header>
     </div>
